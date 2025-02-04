a11a390b53f326622558b8bc4346a4d799558386
Ryohskay <r.nagata.21@abdn.ac.uk>
2025-02-04 16:03:38 +0000

    add file saving function and document potential errors

--- scraper/main.py
+++ scraper/main.py
@@ -1,6 +1,7 @@
 """The main scraping script to get Syriac texts from CAL."""
 
 from urllib.parse import urlencode
+from pathlib import Path
 import urllib3
 
 base_url = "https://cal.huc.edu/"
@@ -17,6 +18,7 @@ def get_a_chapter(
         display_in: str="Latin"):
     """Query the https://cal.huc.edu/get_a_chapter.php endpoint with given parameters.
 
+    If no section is specified, the whole book will be fetched.
     Returns the responded html after decoding in UTF-8.
 
     pool: urllib3.PoolManager
@@ -37,6 +39,10 @@ def get_a_chapter(
         Corresponds to the `cset` URL query parameter.
         "R", "L", "Latin" are for Latin Alphabets, "S", "Syriac" for Syriac scripts.
         Note that "S" option displays Estrangelo in OT but Serto in NT.
+
+    ERRORS:
+        uses urllib.parse.urlencode() without try/catch.
+        uses urllib3.PoolManager.request() without try/catch.
     """
 
     params = {
@@ -54,7 +60,44 @@ def get_a_chapter(
     return res.data.decode("utf-8")  # return decoded text from response text html
 
 
+def get_and_save(
+        fpath: str,
+        pool_mgr: urllib3.PoolManager,
+        book_id: str="62001", 
+        section: str=None, 
+        display_in: str="Latin"
+        ):
+    """Query using get_a_chapter, save the resulting HTML in a file.
+    
+    See get_a_chapter function for detailed requirements of:
+        pool_mgr, book_id, section, display_in
+
+    If no section is specified, the whole book will be fetched.
+
+    fpath: str
+        The name of the file to save the data fetched from CAL.
+        It's passed to pathlib, so it can handle both POSIX and
+        MS Windows paths.
+
+    ERRORS:
+        raises ValueError if the path in fpath param is not a file.
+        raises FileExistsError if the save file specified in fpath param already exists.
+        uses pathlib.Path.write_text() without try/catch.
+    """
+    qr = get_a_chapter(pool_mgr, book_id, section, display_in)
+    dest = Path(fpath)
+    
+    if dest.exists() and not dest.is_file():
+        raise ValueError("The specified path " + fpath + " exists, and it is not a file path.")
+
+    # Avoid overwriting files
+    if dest.exists():
+        raise FileExistsError("The file " + fpath + " exists, and we don't wanna overwrite!")
+    
+    dest.write_text(qr)
+
+
 if __name__ == "__main__":
     http = urllib3.PoolManager()
-    qr = get_a_chapter(http, book_id="62040", section="01", display_in="R")
-    print(qr)
+    get_and_save("./example.html", http)
+    print("Process Complete!")
