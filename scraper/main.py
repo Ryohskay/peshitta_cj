81e11a4ca72da86f1a08c3c63bbd0f91adc42deb
Ryohskay <r.nagata.21@abdn.ac.uk>
2025-02-04 15:10:31 +0000

    start working on CAL scraper

---
+++ scraper/main.py
@@ -0,0 +1,60 @@
+"""The main scraping script to get Syriac texts from CAL."""
+
+from urllib.parse import urlencode
+import urllib3
+
+base_url = "https://cal.huc.edu/"
+
+
+# https://cal.huc.edu/showsubtexts.php?subtext=62040&cset=Latin
+
+# https://cal.huc.edu/get_a_chapter.php?file=62040&cset=
+
+def get_a_chapter(
+        pool: urllib3.PoolManager,
+        book_id: str="62001", 
+        section: str=None, 
+        display_in: str="Latin"):
+    """Query the https://cal.huc.edu/get_a_chapter.php endpoint with given parameters.
+
+    Returns the responded html after decoding in UTF-8.
+
+    pool: urllib3.PoolManager
+        Connection pool for urllib3.
+
+    book_id: "%5d" (string of five-digit numeral)
+        Corresponds to `file` URL query parameter.
+        Must be a string of five-digit numeral to avoid passing "00001" as 1.
+        Defaults to OT Peshitta Genesis.
+
+    section: "%2d" (string of two-digit numeral)
+        Corresponds to `sub` URL query parameter.
+        It must be in the form of "01", "02", ... etc. to work correctly.
+        Defaults to None, in which case the whole book will be fetched.
+
+    display_in: ["R", "L", "Latin", "S", "Syriac"]
+        Defines the writing system (alphabets) in which to display the content.
+        Corresponds to the `cset` URL query parameter.
+        "R", "L", "Latin" are for Latin Alphabets, "S", "Syriac" for Syriac scripts.
+        Note that "S" option displays Estrangelo in OT but Serto in NT.
+    """
+
+    params = {
+        "file": book_id,
+        "cset": display_in
+    }
+
+    if section is not None:
+        # Add "sub" query param
+        params.update({"sub": section})
+
+    # Code adapted from https://www.geeksforgeeks.org/how-to-pass-parameters-in-url-with-python/
+    query_url = base_url + "get_a_chapter.php?" + urlencode(params)
+    res = pool.request("GET", query_url)
+    return res.data.decode("utf-8")  # return decoded text from response text html
+
+
+if __name__ == "__main__":
+    http = urllib3.PoolManager()
+    qr = get_a_chapter(http, book_id="62040", section="01", display_in="R")
+    print(qr)
