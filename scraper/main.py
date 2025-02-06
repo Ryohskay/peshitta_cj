ddc7e480b3e3a19cb4b51d1276ff9effdc89e4bf
Ryohskay <r.nagata.21@abdn.ac.uk>
2025-02-06 23:55:48 +0000

    start implementing text cleaning function

--- scraper/main.py
+++ scraper/main.py
@@ -1,16 +1,40 @@
 """The main scraping script to get Syriac texts from CAL."""
 
 from bs4 import BeautifulSoup
-from cal_handler import pool_init, get_and_save
-from pathlib import Path
+from cal_handler import pool_init, get_a_chapter
+# from pathlib import Path
+import re
+
+
+def clean_word(text: str):
+    # TODO: deal with things like:
+    # wpr:$)/w)p
+    # pr:$)#3#/,
+
+    # TODO: deal with things like:
+    # \slqw
+    # (mh
+    # /#3#/,
+
+    if "#" in text and "<" in text:  # If the word is surrounded by "< >"
+        text = text.replace("<").replace(">")
+    elif "#" in text and "/" in text:
+        text = re.sub("/.+/", "")
+    text = re.sub("#.+#", text.strip())  # remove "#3#"
+
 
 http = pool_init()
-save_f = Path("./out/example.html")
-get_and_save(save_f, http, allow_overwrite=True)
+results = get_a_chapter(http)
 
-soup = BeautifulSoup(save_f.open(), 'html.parser')
+soup = BeautifulSoup(results, 'html.parser')
 
+lines = []
 for link in soup.find_all('a'):
-    print(link)
+    lis = []
+    if "getlex" in link["href"]:
+        word = clean_word(link.text)
+        lis.append(link.text)
+    else:
+        lines.append(lis)
 
 print("Process Complete!")
