796e99052e3b9e1b8dd9bad451c853dee4c2e61c
Ryohskay <learnerryo@gmail.com>
2025-03-30 20:55:12 +0100

    fix main.py

--- scraper/main.py
+++ scraper/main.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup, Tag
 from cal_handler import pool_init, get_a_chapter, follow_link, get_verse_url
 from pathlib import Path
 import re
+import time
+import json
 
 
 def count_char(target: str, source: str) -> int:
@@ -15,8 +17,29 @@ def count_char(target: str, source: str) -> int:
     return num
 
 
+def get_lemma_line(markup_tag: Tag) -> str | None:
+    """Get the line in the lexicon entry where lemma is listed.
+
+    markup_tag: Tag
+        result(s) returned by bs4's find() or find_all(),
+        an excerpt of a HTML document. 
+    """
+    stripped = markup_tag.text.strip()
+    # remove trailing spaces and self-standing periods.
+    # idk why but CAL places periods outside of any html tags,
+    # which the HTML parser considers as a raw string under
+    # the body tag.
+    in_tag_txt = re.sub("^\\.$", "", stripped)
+    if markup_tag.name is None and in_tag_txt != '':
+        # if the content of body tag is not within any tag
+        # but a raw string directly under body tag
+        # (this is how CAL lists lemmata in lexcicon entries)
+        return in_tag_txt
+    return None
+
+
 def is_lemma(lex: str) -> bool:
-    """Returns if given string is a lemma in a CAL lexicon entry."""
+    """Verifies if given string is a valid lemma."""
     anomalous_words = [
             "part of previous word",
             "non-Aramaic or fragmentary",
@@ -30,59 +53,76 @@ def is_lemma(lex: str) -> bool:
     return True
 
 
-def find_lemma(markup_tag: Tag):
-    """Find the lemma from given HTML excerpt.
+def extract_lemma_annot(tag_txt: str) -> tuple[str | None, str | None]:
+    """Extract the lemma and annotation from given document excerpt.
     
-    markup_tag: Tag
-        result(s) returned by bs4's find() or find_all()
+    Note that the lemma annotations may not provide POS at all,
+    e.g. https://cal.huc.edu/getlex.php?coord=620010102&word=9
+
+    tag_txt: str
+        string returned by get_lemma_line()
     """
-    if markup_tag.name is None:
-        # if the content of body tag is not within any tag
-        # but a raw string (this is how CAL lists lemmata)
-        stripped = markup_tag.text.strip()
-        # remove trailing periods lying outside the tags
-        res = re.sub("^\\.$", "", stripped)
-        if res != "" and is_lemma(res):
-            # get the lemma and remove POS description etc.
-            res = res.split(' ')[0]
-            # remove numbering appended to lemma, like "???#2"
-            # and replace "@" in compound words with a space
-            return re.sub("#\\d", "", res).replace("@", " ")
+    if is_lemma(tag_txt):
+        # get the lemma and remove POS description etc.
+        split_line = tag_txt.split(' ')
+        lemma = split_line[0]
+        # remove numbering appended to lemma, like "???#2"
+        # and replace "@" in compound words with a space
+        lemma = re.sub("#\\d", "", lemma).replace("@", " ")
+        # record the annotations if they exist
+        if len(split_line) > 1:
+            annot = ' '.join(split_line[1:]) # lemma annotations like POS, morphology
+        else:
+            print(f"INFO: Annotation not given for lemma: {lemma}")
+            annot = None
+        return (lemma, annot)
+    return (None, tag_txt)
 
 
 def is_lex(url: str) -> bool:
+    """Check if the url leads to a lexicon entry."""
     if "getlex" in url:
         return True
     return False
 
 
+def is_empty_verse(words: list) -> bool:
+    """Check if the verse is all None."""
+    for w in words:
+        if w is not None:
+            return False
+    return True
+
+
 if __name__ == "__main__":
 
     target_books = [
-            # ("Matthew", "62040"),
-            # ("Mark", "62041"),
-            # ("Luke", "62042"),
-            # ("John", "62043"),
-            # ("Genesis", "62001"),
-            # ("Exodus", "62002"),
-            # ("Acts", "62044"),
-            # ("Deuteronomy", "62005"),
-            # ("Joshua", "62006"),
-            # ("Judges", "62007"),
-            # ("1_Samuel", "62008"),
-            # ("2_Samuel", "62009"),
-            # ("1_Kings", "62010"),
-            # ("2_Kings", "62011"),
-            # ("Ruth", "62030"),
-            # ("Esther", "62034"),
-            # ("Ezra", "62036"),
-            # ("Nehemiah", "62037"),
-            # ("1_Chronicles", "62038"),
-            # ("2_Chronicles", "62039"),
+            ("Matthew", "62040"),
+            ("Mark", "62041"),
+            ("Luke", "62042"),
+            ("John", "62043"),
+            ("Genesis", "62001"),
+            ("Exodus", "62002"),
+            ("Acts", "62044"),
+            ("Deuteronomy", "62005"),
+            ("Joshua", "62006"),
+            ("Judges", "62007"),
+            ("1_Samuel", "62008"),
+            ("2_Samuel", "62009"),
+            ("1_Kings", "62010"),
+            ("2_Kings", "62011"),
+            ("Ruth", "62030"),
+            ("Esther", "62034"),
+            ("Ezra", "62036"),
+            ("Nehemiah", "62037"),
+            ("1_Chronicles", "62038"),
+            ("2_Chronicles", "62039"),
             ("1_Maccabees", "62078"),
             ]
 
-    #book_idx = "62006"
+    # book_idx = "62006"
+
+    print(time.asctime())
 
     http = pool_init()
     for book in target_books:
@@ -96,29 +136,33 @@ if __name__ == "__main__":
         # Use lxml parser to correctly handle raw texts in body tag
         soup = BeautifulSoup(result, 'lxml')
 
+        # book-level vars
         # lists to store results
         verses = []
         raw_verses = [] # aggregate list of inflected texts from all verses
-        raw_words = [] # inflected form of words in each verse, as a list
-        word_lis = []  # lemmatised words of each verse as a list of lemmata
         verse_ref_nums = [] # the reference numbers for each verse
         empty_verses = [] # verse reference to verses where no lemma could be
                           # retrieved
         error_lines = [] # lines where DB error is suspected
         xx_lines = [] # lines where the reference is XX
-        verse_urls = []
+        verse_urls = [] # list of verse urls 
+        verse_annots = [] # aggregate list of verse annotations
 
-        lex_url = ""
-        verse_url = ""
-
-        # flags
-        is_verse_line = False
-
-        # counters
+        # book-level counters
         num_slashes = 0
         num_lemmata = 0
         num_no_lemma = 0
         num_links = 0
+
+        # verse-level vars
+        word_lis = []  # lemmatised words of each verse as a list of lemmata
+        raw_words = [] # inflected form of words in each verse, as a list
+        annot_lis = [] # annotations of the lemma, as a list of str
+        lex_url = ""
+        verse_url = ""
+
+        # verse-level flags
+        is_verse_line = False
 
         for table_data in soup.find_all('td'):
             if "valign" in table_data.attrs.keys() and table_data["valign"] == "top":
@@ -127,8 +171,13 @@ if __name__ == "__main__":
                 verse_ref = re.match("\\d\\d:\\d\\d", table_data.text)
                 if verse_ref is not None:
                     vid = verse_ref.group()
-                    verse_ref_nums.append(vid)
                     print(vid)
+                    v_refs = vid.split(':')
+                    reference = (
+                            f'{book[0]} Chapter {v_refs[0]}'
+                            + f' Verse {v_refs[1]}'
+                            )
+                    verse_ref_nums.append(reference)
                     is_verse_line = True
                 elif "d.:ne" in table_data.text:
                     # probably some error in the database, skip the line
@@ -172,30 +221,38 @@ if __name__ == "__main__":
                         lex_body = lex_soup.body
 
                         for body_child in lex_body.children:
-                            res = find_lemma(body_child)
-                            if res is not None:
+                            # for each DOM object directly under the body tag
+                            lemma_line = get_lemma_line(body_child)
+                            if lemma_line is not None:
+                                res, annot = extract_lemma_annot(lemma_line)
                                 word_lis.append(res)
+                                annot_lis.append(annot)
+                                if res is None:
+                                    num_no_lemma += 1
                     else:
                         word_lis.append(None)
+                        annot_lis.append(None)
                         num_no_lemma += 1
 
                 # extract the hyperlink to verse from the lex_url
                 if verse_url != "":
-                    verse_url = get_verse_url(lex_url)
-                    verse_urls.append(verse_url)
+                    extracted = get_verse_url(verse_url)
+                    verse_urls.append(extracted)
+
 
                 # when all links in one table cell has been explored,
                 # push the list of scraped lemmata to verses[]
-                #if len(word_lis) > 0:
                 print(word_lis)
                 num_lemmata = num_lemmata + len(word_lis)
-                raw_verses.append(raw_words)
                 verses.append(word_lis)
-                if len(word_lis) == 0:
+                raw_verses.append(raw_words)
+                verse_annots.append(annot_lis)
+                if len(word_lis) == 0 or is_empty_verse(word_lis):
                     empty_verses.append(verse_ref_nums[-1])
-                # reset the lists
+                # reset the verse-level lists
                 word_lis = []
                 raw_words = []
+                annot_lis = []
 
         # the number of scribal variances is number of slashes in raw text divided by 2
         num_variances = num_slashes / 2
@@ -209,22 +266,43 @@ if __name__ == "__main__":
                              )
             print(f"empty verses: {empty_verses}")
 
-        # format the data in a string of CSV format
-        formatted_data = "Verse Ref. No.,Verse URL,Raw Text,Lemmatised Text\n"
-        for i in range(len(verses)):
-            indices = verse_ref_nums[i].split(":")
-            raw_txt_verse = ' '.join(raw_verses[i])
-            lemma_verse = ' '.join(verses[i])
-            formatted_data = formatted_data + f'"Chapter {indices[0]} verse {indices[1]}","{verse_urls[i]}","{raw_txt_verse}","{lemma_verse}"\n'
+        # # format the data in a string of CSV format
+        # formatted_data = (
+        #         'Verse Ref. No.,Verse URL,Raw Text,'
+        #         + 'Lemmatised Text,Lemma Annotations\n'
+        #         )
+        # print(verse_annots)
+        # for i in range(len(verses)):
+        #     indices = verse_ref_nums[i].split(':')
+        #     raw_txt_verse = ' '.join(raw_verses[i])
+        #     lemma_verse = '#/#'.join(verses[i])
+        #     annot_verse = '#/#'.join(verse_annots[i])
+        #     formatted_data = (
+        #             formatted_data
+        #             + f'"Chapter {indices[0]} verse {indices[1]}",'
+        #             + '"{verse_urls[i]}","{raw_txt_verse}",'
+        #             + '"{lemma_verse}","{annot_verse}"\n'
+        #             )
 
         # Store the scraped lines into a csv file
-        p = Path(f"./out/scraper_results_{book[0]}.csv")
-        p.write_text(formatted_data)
+        with Path(f'./out/scraper_results_{book[0]}.json').open(mode="w") as fp:
+            book_data = {
+                    'book_title': book[0],
+                    'verse_refs': verse_ref_nums,
+                    'verse_urls': verse_urls,
+                    'raw_text_verses': raw_verses,
+                    'lemmatised_verses': verses,
+                    'lemma_annotations': verse_annots
+                    }
+            json.dump(book_data, fp)
 
         print(f"Done scraping for book {book[0]}")
+        print(time.asctime())
         print("--RESULTS--")
         print(f"number of hyperlinks found in the document: {num_links}")
         print(f"number of lemmata: {num_lemmata}")
+        print(f"number of empty lemmata: {num_no_lemma}")
+        print(f"number of real lemmata: {num_lemmata - num_no_lemma}")
         print(f"number of scribal variants: {num_variances}")
         print(f"number of verses: {len(verses)}")
         print(f"number of empty verses: {len(empty_verses)}")
