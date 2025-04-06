bfdd9e4e58b5f224d229e403a8f4f5ae201a1ce5
Ryohskay <r.nagata.21@abdn.ac.uk>
2025-04-06 23:40:56 +0100

    update add_license.sh

--- add_license.sh
+++ add_license.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-echo -e "\"\"\"$(bat LICENSE)\"\"\"\n$(bat $1)" > $1
+echo -e "$(bat LICENSE)\n$(bat $1)" > $1
+echo "Added the LICENSE statement to file: $1."
+echo "Don't forget to comment out the LICENSE!"
