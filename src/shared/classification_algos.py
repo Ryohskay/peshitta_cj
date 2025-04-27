4ac29d0a386a29a9661dfbd5ad5ff3006f6c79fc
Ryohskay <r.nagata.21@abdn.ac.uk>
2025-04-27 03:21:27 +0100

    add classification algorithm dict

---
+++ src/shared/classification_algos.py
@@ -0,0 +1,35 @@
+"""Definition of the match between the algorithms and their name."""
+
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.base import BaseEstimator
+
+from src.classifier.wrappers import BoWEstimator
+
+ALGORITHMS = {
+    "mnb": {"algo": MultinomialNB},
+    "knn": {"algo": KNeighborsClassifier},
+    "rf": {"algo": RandomForestClassifier},
+}
+
+def get_algo_by_name(name: str, **kwargs) -> BaseEstimator: # noqa: ANN003
+    """Get the algorithm by its name.
+
+    Args:
+        name: The name of the algorithm.
+        kwargs: Additional arguments to pass to the initialiser of
+            the algorithm.
+
+    Returns:
+        An object for the algorithm, an instance of `BaseEstimator` from
+        scikit-learn.
+
+    Raises:
+        ValueError: If the algorithm is not defined.
+    """
+    if name not in ALGORITHMS:
+        msg = (f"Algorithm {name} is not defined. Available algorithms are: "
+        + f"{[(algo, ALGORITHMS[algo]["description"]) for algo in ALGORITHMS]}")
+        raise ValueError(msg)
+    return ALGORITHMS[name]["algo"](**kwargs)
