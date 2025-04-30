77a582b47524ee535ab7b3c80b43ccc9f666fdbf
Ryohskay <91414688+Ryohskay@users.noreply.github.com>
2025-05-01 00:10:11 +0100

    update to count no. of words per book

--- src/scripts/save_mlm_text_data.py
+++ src/scripts/save_mlm_text_data.py
@@ -100,16 +100,20 @@ if __name__ == "__main__":
     print(f"{len(ot_verses)} books")
     save_txt = ""
     for book in ot_verses:
-        print(f"Book of {book}: {len(ot_verses[book])} verses")
+        num_words = 0
         for v in ot_verses[book]:
+            num_words += len(v.words)
             save_txt += f"{' '.join(v.get_translit_words())}\n"
+        print(f"Book of {book}: {len(ot_verses[book])} verses / {num_words} words")        
     Path("src/neural/data/ot_mlm_data.txt").write_text(save_txt, encoding="utf-8")
     
     save_txt = ""
     print("NT")
     print(f"{len(nt_verses)} books")
     for book in nt_verses:
-        print(f"Book of {book}: {len(nt_verses[book])} verses")
+        num_words = 0
         for v in nt_verses[book]:
+            num_words += len(v.words)
             save_txt += f"{' '.join(v.get_translit_words())}\n"    
+        print(f"Book of {book}: {len(nt_verses[book])} verses / {num_words} words")
     Path("src/neural/data/nt_mlm_data.txt").write_text(save_txt, encoding="utf-8")
