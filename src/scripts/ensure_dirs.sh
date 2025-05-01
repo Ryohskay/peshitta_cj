d6f43b7a625d52b9c7a376fe33f8c22908933383
Ryohskay <91414688+Ryohskay@users.noreply.github.com>
2025-05-01 01:00:23 +0100

    add script to ensure dir existence

---
+++ src/scripts/ensure_dirs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+PROJ_PATH=$HOME/peshitta_cj_ssh
+mkdir -p $PROJ_PATH/out/cross_validation
+mkdir -p $PROJ_PATH/src/classifier/out
--- src/scripts/submit_etcbc_eval.sh
+++ src/scripts/submit_etcbc_eval.sh
@@ -16,6 +16,8 @@ source /home/u05rn21/venv/bin/activate
 python --version
 
 cd /home/u05rn21/peshitta_cj_ssh
+src/scripts/ensure_dir.sh
+
 python -m pip install --upgrade pip
 python -m pip install numpy
 python -m pip install scikit-learn
